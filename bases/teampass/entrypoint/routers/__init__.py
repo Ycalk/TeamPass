8fccd7de1fa7d6077abb2d8ddc20fd4bd035e895
ycalk <alosev752@gmail.com>
2026-04-19 22:28:55 +0500

    fix(users): переименование рутера user -> users

--- bases/teampass/entrypoint/routers/__init__.py
+++ bases/teampass/entrypoint/routers/__init__.py
@@ -1,4 +1,4 @@
 from .auth import router as auth_router
-from .user import router as user_router
+from .users import router as users_router
 
-__all__ = ["auth_router", "user_router"]
+__all__ = ["auth_router", "users_router"]
